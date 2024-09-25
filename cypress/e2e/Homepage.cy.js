b9d966d4cb06b93e861a348ad332384f49119896
Mehak waheed <mehak.waheed@clustox.com>
2024-09-25 15:55:38 +0500

    Your commit message here

---
+++ cypress/e2e/Homepage.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+
+describe('Home Page Tests', () => {
+  beforeEach(() => {
+      cy.visit('https://price92.pk');
+  });
+
+  it('should have a title element', () => {
+      cy.title().should('exist');
+  });
+
+
+  it('should have a meta description', () => {
+      cy.get('meta[name="description"]').should('exist');
+  });
+
+  it('should contain a header', () => {
+      cy.get('h1').should('exist');
+  });
+
+  it('should contain a canonical url', () => {
+    cy.get('link[rel="canonical"]').should('have.attr', 'href', 'https://price92.pk');
+});
+});
---
+++ cypress/e2e/Upcoming_Mobiles.cy.js
@@ -0,0 +1,65 @@
+/// <reference types="cypress" />
+
+const brands = [
+    'https://price92.pk/mobiles/apple-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/samsung-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/oukitel-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/umidigi-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/nothing-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/sharp-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/xiaomi-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/oppo-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/infinix-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/tecno-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/vivo-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/itel-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/realme-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/nokia-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/zte-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/sparx-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/dcode-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/huawei-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/google-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/honor-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/oneplus-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/meizu-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/lenovo-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/qmobile-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/sony-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/motorola-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/htc-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/telenor-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/alcatel-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/lg-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/tcl-phones-price-in-pakistan',
+    'https://price92.pk/mobiles/club-phones-price-in-pakistan',
+];
+
+describe('Tests for Multiple Brands', function() {
+  brands.forEach((brandUrl) => {
+    context(`Testing ${brandUrl}`, function() {
+      beforeEach(function() {
+          cy.visit(brandUrl);
+      });
+
+      it('should have a title element', function() {
+          cy.title().should('exist');
+      });
+
+
+      it('should have a meta description', function() {
+          cy.get('meta[name="description"]').should('exist');
+      });
+
+
+      it('should contain a header', function() {
+          cy.get('h1').should('exist');
+      });
+
+      it('should have a canonical link', function() {
+          cy.get('link[rel="canonical"]').should('exist');
+      });
+
+    });
+  });
+});
